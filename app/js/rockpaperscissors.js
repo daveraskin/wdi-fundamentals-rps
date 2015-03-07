63445ad6dd141c183a6694269aee6db8a67a90c1
Dave <draskal2@gmail.com>
2015-03-07 10:28:02 -0800

    got it all to work

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,7 +1,3 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
@@ -17,39 +13,79 @@ function randomPlay() {
         return "scissors";
     }
 }
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
+
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    
     return move || getInput();
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    
     return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
+function getWinner(playerMove, computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove == "scissors" && computerMove == "paper") {
+        winner = "player";
+    }
+    else if (playerMove == "paper" && computerMove == "rock") {
+        winner = "player";
+    }
+    else if (playerMove == "rock" && computerMove == "scissors") {
+        winner = "player";
+    }
+    else if (playerMove == "paper" && computerMove == "scissors") {
+        winner = "computer";
+    }
+    else if (playerMove == "rock" && computerMove == "paper") {
+        winner = "computer";
+    }
+    else if (playerMove == "scissors" && computerMove == "rock") {
+        winner = "computer";
+    }
+    else if (playerMove == "paper" && computerMove == "paper") {
+        console.log ("There's been a tie!");
+    }
+    else if (playerMove == "rock" && computerMove == "rock") {
+        console.log ("There's been a tie!");
+    }
+    else if (playerMove == "scissors" && computerMove == "scissors") {
+        console.log ("There's been a tie!");
+    }
+    else {console.log ("Please choose either 'rock', 'paper', or 'scissors'."); }
+    
+   
+   
+    
     return winner;
+    
 }
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        var winner = getWinner(playerMove,computerMove);
+        if (winner == "player") {
+                 playerWins += 1;
+                 console.log("You've Won!");
+                  console.log("The score is currently " + playerWins + " to " + computerWins );
+            }
+        else if (winner == "computer") {
+                computerWins += 1;
+                console.log ("You've lost to the computer");
+                console.log("The score is currently " + playerWins + " to " + computerWins );
+            }
+        else {  console.log("The score is currently " + playerWins + " to " + computerWins );
+
+        }
+    }
 }
 
+playToFive();
